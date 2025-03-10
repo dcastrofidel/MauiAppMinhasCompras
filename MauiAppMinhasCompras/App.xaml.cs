9b5c6e7d02243b683d50d2d734aeabed7c3edc27
dcastrofidel <dgarcia.fidel@gmail.com>
2025-03-10 13:48:59 -0300

    instanciamento da classe(SQLietDataBaseHelper) para acesso ao CRUD desenvolvido na agenda 2, toranndo assim a classe acessível juntamente com nossos métodos e pacotes NUGET

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,7 +1,29 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {//Instanciamento da classe do SQLite database helper para acessar o Crud, as API,pacotes Nuget e metodos, tornando a classe(SQLiteDataBaseHelper) dispónivel para acesso em todo o app, fazendo assim alterações em qualquer parte dele
+            get
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            
+            }
+        }
         public App()
         {
             InitializeComponent();
