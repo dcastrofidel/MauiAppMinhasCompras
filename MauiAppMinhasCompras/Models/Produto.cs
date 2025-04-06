c682fbe953b3ea2f1e873456a5057fed891c2a87
dcastrofidel <dgarcia.fidel@gmail.com>
2025-04-06 16:22:38 -0300

    validação de inserção de dados em descrição, quantidade e preço, correção de bug na lsitagem duplicada

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -1,14 +1,74 @@
 ﻿using SQLite;
+using System.Globalization;
 
 namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _quantidade;
+        double _preco;
+
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public string Descricao {
+
+            get => _descricao; 
+            
+            set
+            {
+                if (value == null) 
+                {
+                    throw new Exception("Por Favor, preencha a descrição");
+                }
+
+                _descricao = value;
+
+            }
+                
+                
+        }
+
+
+
+        public double Quantidade {
+
+            get => _quantidade;
+
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Por Favor, preencha a quantidade");
+                }
+
+                _quantidade = value;
+
+            }
+
+
+        }
+
+
+        public double Preco  {
+
+            get => _preco;
+
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Por Favor, preencha o preço");
+                }
+
+                _preco = value;
+
+            }
+
+
+        }
+
+
         public double Total { get => Quantidade * Preco; }
     }
 }
